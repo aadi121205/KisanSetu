8c368a71eed97f601ed6be2eed231c0c14300698
Aadi <aaditya12120205@gmail.com>
2024-05-20 02:23:18 +0530

    git fixs

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavigationBar from './Navbar'; // Adjust the path as necessary
+import NavigationBar from './Comps/Navbar'; // Adjust the path as necessary
 
 const App: React.FC = () => {
   return (
