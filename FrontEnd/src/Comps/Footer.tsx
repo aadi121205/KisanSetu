b3864ed2ef0782d89f4bd3e3b2348baa7cd34d7b
Aadi <aaditya12120205@gmail.com>
2024-05-20 05:49:16 +0530

    Automatic commit

--- FrontEnd/src/Comps/Footer.tsx
+++ FrontEnd/src/Comps/Footer.tsx
@@ -5,8 +5,28 @@ const Footer: React.FC = () => {
     return (
         <footer className="bg-dark text-white py-3 mt-5">
             <Container>
-                <Row>
-                    <Col className="text-center">
+                <Row className="mb-3">
+                    <Col className="text-center text-lg">
+                        <p className="mb-0">Your Company Name</p>
+                    </Col>
+                </Row>
+                <Row className="mb-3">
+                    <Col className="text-center text-lg">
+                        <p className="mb-0">123 Main Street, New York, NY 10001</p>
+                    </Col>
+                    <Col className="text-center text-lg">
+                        <p className="mb-0">
+                            <a href="tel:555-555-5555">555-555-5555</a>
+                        </p>
+                    </Col>
+                    <Col className="text-center text-lg">
+                        <p className="mb-0">
+                            <a href="mailto:aaditya.tec@gmail.com">aaditya.tec@gmail.com</a>
+                        </p>
+                    </Col>
+                </Row>
+                <Row className="mb-3">
+                    <Col className="text-center text-lg">
                         <p className="mb-0">© 2022 Your Company Name. All rights reserved.</p>
                     </Col>
                 </Row>
