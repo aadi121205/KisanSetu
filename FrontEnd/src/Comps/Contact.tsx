76ae3d366ef4e68f95c043c55d110dee6503c508
Aadi <aaditya12120205@gmail.com>
2024-05-20 06:49:52 +0530

    Automatic commit

--- FrontEnd/src/Comps/Contact.tsx
+++ FrontEnd/src/Comps/Contact.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Contact: React.FC = () => {
     return (
         <div>
             <h1>Contact Us</h1>
             <p>This is the contact page.</p>
+            <div>
+                <MapContainer center={[40.7128, -74.0060]} zoom={13}>
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    <Marker position={[40.7128, -74.0060]}>
+                        <Popup>
+                            Your Company Name<br />123 Main Street, New York, NY 10001
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
         </div>
     );
 };
